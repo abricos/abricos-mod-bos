e67479803e239768b453fedc0e067844bfefce7b
Alexander Kuzmin <roosit@abricos.org>
2011-02-28 14:42:19 +0000

    Bos UI: набросок переключения между страницами

--- js/os.js
+++ js/os.js
@@ -32,6 +32,27 @@ Component.entryPoint = function(){
 		alert('logout');
 	};
 	
+	var buildTemplate = function(w, templates){
+		var TM = TMG.build(templates), T = TM.data, TId = TM.idManager;
+		w._TM = TM; w._T = T; w._TId = TId;
+	};
+	
+	var WaitPanel = function(){
+		WaitPanel.superclass.constructor.call(this, {
+			fixedcenter: true, width: '350px',
+			modal: true
+		});
+	};
+	YAHOO.extend(WaitPanel, Brick.widget.Panel, {
+		initTemplate: function(){
+			buildTemplate(this, 'waitpanel');
+			return this._TM.replace('waitpanel', {
+				// 'tl': this.prjTitle
+			});
+		}
+	});
+	NS.WaitPanel = WaitPanel;
+	
 	/**
 	 * Приложение BOS.
 	 * 
@@ -152,7 +173,15 @@ Component.entryPoint = function(){
 		if (el.tagName == 'A'){ return el; }
         return findA(el.parentNode, cnt+1);
 	};
-
+	
+	/*
+	var Page = function(){
+		
+	};
+	Page.prototype = {
+		
+	};
+	/**/
 	
 	var Workspace = function(){
 		this.init();
@@ -163,15 +192,16 @@ Component.entryPoint = function(){
 		init: function(){
 			Workspace.instance = this;
 			
-			this.selectedApp = null;
+			this.selectedKey = 'home';
 			
-			var TM = TMG.build('menuitem,page'), T = TM.data, TId = TM.idManager;
+			var TM = TMG.build('menulist,menuitem,page'), T = TM.data, TId = TM.idManager;
 			this._T = T; this._TId = TId; this._TM = TM;
 			
 			var container = Dom.get("home");
 			this.container = container;
 			
-
+			this.pages = [{'key': 'home', 'element': container}];
+			
 			var list = [];
 			// сформировать список модулей имеющих компонент 'app' в наличие
 			for (var m in Brick.Modules){
@@ -205,34 +235,169 @@ Component.entryPoint = function(){
 			});
 		},
 		
-		_setWorkspaceSize: function(){
-			var r = Dom.getClientRegion();
-			var el = this.container;
-			
-            Dom.setStyle(el, "width", (r.width)+'px');
-            Dom.setStyle(el, "height", (r.height-32)+'px');
-            this.orderLabelPosition();
-		},
-		
 		render: function(){
 			var __self = this, lst = "", TM = this._TM;
 			NS.ApplicationManager.each(function(app){
 				lst += TM.replace('menuitem', {
 					'id': app.id,
-					'name': app.name,
+					'mod': app.moduleName,
+					'comp': app.entryComponent,
+					'page': app.entryPoint,
 					'icon': app.icon,
 					'title': app.getTitle()
 				});
 			});
 			
-			this.container.innerHTML = lst;
+			this.container.innerHTML = TM.replace('menulist', {'rows': lst});
 			
 			var __self = this;
 			var bookmarkedSection = H.getBookmarkedState("app") || "home";
-			H.register("app", bookmarkedSection, function (appid) {
-				__self.openAppById(appid);
+			H.register("app", bookmarkedSection, function (key) {
+				__self.navigate(key);
 			});
 			YAHOO.util.History.initialize("yui-history-field", "yui-history-iframe");
+		},
+		
+		navigate: function(key){
+			var arr = key.split('/'),
+				mod = arr[0],
+				comp = arr[1] || '',
+				page = arr[2] || '',
+				prm1 = arr[3] || '', prm2=arr[4]||'', prm3=arr[5]||'', prm4=arr[6]||'', prm5=arr[7]||'';
+			
+			if (this.pageExist(key)){
+				this.showPage(key);
+				return; 
+			}
+			if (!Brick.componentExists(mod, comp)){ return; }
+			
+			var wait = new WaitPanel(),
+				TM = this._TM,
+				__self = this;
+			
+			var fire = function(){
+				
+				wait.close();
+				var fn = Brick.mod[mod]['API'][page];
+				if (!L.isFunction(fn)){ return; }
+				
+				var div = document.createElement('div');
+				div.innerHTML = TM.replace('page', {
+					'id': key.replace(/\//g, '-')
+				});
+				element = div.childNodes[0];
+				Dom.get('pages').appendChild(element);
+				
+				fn(element, prm1, prm2, prm3, prm4, prm5);
+				__self.addPage(key, element);
+				__self.showPage(key);
+				
+				// Dom.get('home').style.display = 'none';
+				// element.style.display = '';
+			};
+			
+			if (!Brick.componentLoaded(mod, comp)){
+				Brick.Component.API.fireFunction(mod, comp, function(){
+					fire();
+				});
+			}else{
+				fire();
+			}
+		},
+		
+		showPage: function(key){
+			if (this.selectedKey == key){ return; }
+			
+			// reindex pages
+			var ps = this.pages;
+			for (var i=0; i<ps.length;i++){
+				ps[i]['index'] = i;
+			}
+
+			var eBD = Dom.get('bd'),
+				ePS = Dom.get('pages'),
+				rg = Dom.getRegion(eBD),
+				p1 = this.getPage(this.selectedKey), i1 = p1.index, e1 = p1.element,
+				p2 = this.getPage(key), i2 = p2.index, e2 = p2.element;
+			
+			Dom.addClass(eBD, 'movedmode');
+			Dom.addClass(ePS, 'movedmode');
+			
+			var w = rg.width, dx = 50, x = 0;
+
+			Dom.setStyle(ePS, 'width', (w*2+20)+'px');
+			if (i1>i2){
+				Dom.setStyle(ePS, 'left', (-w)+'px');
+				dx = -dx;
+				x = -w;
+			}
+
+			
+			Dom.setStyle(e1, 'float', 'left');
+			Dom.setStyle(e1, 'width', w+'px');
+			
+			Dom.setStyle(e2, 'float', 'left');
+			Dom.setStyle(e2, 'width', w+'px');
+			Dom.setStyle(e2, 'display', '');
+			
+			this.selectedKey = key;
+			
+			var stop = function(){
+				e1.style.display = 'none';
+				Dom.setStyle(e1, 'float', '');
+				Dom.setStyle(e1, 'width', '');
+				Dom.setStyle(e2, 'float', '');
+				Dom.setStyle(e2, 'width', '');
+				Dom.setStyle(ePS, 'width', '');
+				Dom.setStyle(ePS, 'left', '');
+				
+				Dom.removeClass(eBD, 'movedmode');
+				Dom.removeClass(ePS, 'movedmode');
+			};
+			
+			var thread = setInterval(function(){
+				if (
+//						(xc > xn && xn > x) || // движение вперед 
+//						(xc < xn && xn < x) // движение назад
+						(i1<i2 && x < -(w-dx)) ||
+						(i1>i2 && x >= 0)
+					){
+					clearInterval(thread);
+					stop();
+					return;
+				}
+				x -= dx;
+				
+				Dom.setStyle(ePS, 'left', (x)+'px');
+			}, 10);
+			
+		},
+		
+		pageExist: function(key){
+			return !L.isNull(this.getPage(key));
+		},
+		
+		getPage: function(key){
+			var ps = this.pages;
+			for (var i in ps){
+				if (ps[i]['key'] == key){
+					return ps[i];
+				}
+			}
+			return null;
+		},
+		
+		addPage: function(key, element){
+			if (this.pageExist(key)){ return; }
+			var ps = this.pages;
+			ps[ps.length] = {'key': key, 'element': element};
+		}
+		
+		/*
+		,
+		
+		openPage: function(){
+			
 		},
 		
 		openAppById: function(appid){
@@ -287,7 +452,7 @@ Component.entryPoint = function(){
 				fire();
 			}
 		}
-
+		/**/
 	};
 	NS.Workspace = Workspace;
 	
